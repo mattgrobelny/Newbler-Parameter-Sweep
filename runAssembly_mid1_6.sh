ac47590edab243949dc85c32d2a7d6d7fbd8a878
mattgrobelny <mattgrobelny@gmail.com>
2016-05-21 01:02:00 -0500

    make into dumb script

--- runAssembly_mid1_6.sh
+++ runAssembly_mid1_6.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-for i in {1..6}; do ./nohup `perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid_$i ../bothtrimfiles.fasta ../mid_MID$i.sff &`; done
+`./nohup perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid_1../bothtrimfiles.fasta ~/Data/mid_MID1.sff &`
+`./nohup perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid_2../bothtrimfiles.fasta ~/Data/mid_MID2.sff &`
+`./nohup perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid_3../bothtrimfiles.fasta ~/Data/mid_MID3.sff &`
+`./nohup perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid_4../bothtrimfiles.fasta ~/Data/mid_MID4.sff &`
+`./nohup perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid_5../bothtrimfiles.fasta ~/Data/mid_MID5.sff &`
+`./nohup perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid_6../bothtrimfiles.fasta ~/Data/mid_MID6.sff &`
+
+done 
