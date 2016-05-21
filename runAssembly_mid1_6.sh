b51fe72e95debc7983b35382b644138244245222
mattgrobelny <mattgrobelny@gmail.com>
2016-05-21 00:50:52 -0500

    add var to name

--- runAssembly_mid1_6.sh
+++ runAssembly_mid1_6.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in {1..6}; do ./nohup perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid1_run ../bothtrimfiles.fasta ../mid_MID$i.sff &; done
+for i in {1..6}; do ./nohup perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 Mid_$i ../bothtrimfiles.fasta ../mid_MID$i.sff &; done
