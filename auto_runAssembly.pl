dc9a2d361d8f469d2da00800e477b9f492adfee4
mattgrobelny <mattgrobelny@gmail.com>
2016-06-14 10:55:48 -0500

    Added auto run script

---
+++ auto_runAssembly.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+#use strict;
+use warnings;
+
+for($i=1; $i<=6;$i=$i+1){
+  my $MID_file="mid_MID";
+  my $MID_file_sff= '../'."$MID_file" . $i ."sff";
+  system("perl runAssembly_PS 15 45 5 15 50 5 95 99 1 $MID_file ../bothtrimfiles.fasta $MID_file_sff TRUE TRUE\n");
+}
